fee4d4e5451c2246ce2765aac6538fd08a8d2606
Kary <karina_galvez@epam.com>
2021-08-09 23:37:32 -0500

    Fixed Fragments
    Signed-off-by: Kary <karina_galvez@epam.com>

---
+++ app/src/main/java/com/home/intagracoleneapp/Adapter/MyFotosAdapter.java
@@ -0,0 +1,87 @@
+package com.home.intagracoleneapp.Adapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.cloneappinsta.R;
+import com.home.intagracoleneapp.Fragment.PostDetailFragment;
+
+
+import java.time.Instant;
+import java.util.List;
+
+import Model.Post;
+
+public class MyFotosAdapter extends  RecyclerView.Adapter<MyFotosAdapter.ViewHolder> {
+
+    private Context context ;
+    private List<Post> mPosts;
+    private Instant Glide;
+
+    public MyFotosAdapter(Context context, List<Post> mPosts) {
+        this.context = context;
+        this.mPosts = mPosts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View  view = LayoutInflater.from(context).inflate(R.layout.fotos_item,parent,false);
+
+        return new MyFotosAdapter.ViewHolder(view);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewholder, int i) {
+
+        final Post post = mPosts.get(i);
+        Glide.with(context).load(post.getPostimage()).into(viewholder.post_image);
+
+
+        viewholder.post_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences.Editor editor = context.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit();
+                editor.putString("postid",post.getPostid());
+                editor.apply();
+                ((FragmentActivity)context).getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PostDetailFragment()).commit();
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    public  class  ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView post_image;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            post_image = itemView.findViewById(R.id.post_image);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/home/intagracoleneapp/StartActivity.java
@@ -0,0 +1,62 @@
+package com.home.intagracoleneapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cloneappinsta.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button login,ragister;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+
+            startActivity(new Intent(StartActivity.this,MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.login_btn);
+        ragister = findViewById(R.id.Ragister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent( StartActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        ragister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent( StartActivity.this,RagisterActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+}
